2b9d373c059b12535e1d71d092ffa21fbbabe465
Peitong Liu <vincentliu9816@gmail.com>
2020-03-12 07:34:42 -0400

    None of the regex works so far, had to go primitive. --2020/03/12

--- script.js
+++ script.js
@@ -4,17 +4,15 @@
     Author: Peitong Liu 
     ID: 300913117
 */
-
 "use strict";
-
 //Regex for fields.
+//None of the regex works so far, had to go primitive. --2020/03/12 
 var regexForPhone = /^[0-9]{10}$/;
 var regexCdnZip = /^([A-Z]\d[A-Z]\s?\d[A-Z]\d)$/;
-var regexUsaZip = /^[0-9]{5}(?:-[0-9]{4})?$/;
+var regexUsaZip = /^[0-9]{5}$/;
 var regexEmail = /^[_a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-z]{2,6})$/;
 var regexPwd = /"^[a-zA-Z0-9]{8,12}$/;
 var regexName = /^[A-Z][a-z]{3,19}$/;
-
 function checkFName(){
     var fnInput = document.getElementById("inputFN");
     var fnLabel = document.getElementById("lblFN");
@@ -31,7 +29,6 @@ function checkFName(){
         fnCell.style.background = "rgb(255,233,233)";
     }
 }
-
 function checkLName(){
     var lnInput = document.getElementById("inputLN");    
     var lnLabel = document.getElementById("lblLN");
@@ -48,7 +45,6 @@ function checkLName(){
         lnCell.style.background = "rgb(255,233,233)";
     }
 }
-
 function checkPassword1(){
 	var pw1Input = document.getElementById("inputPW1");
 	var pw1Label = document.getElementById("lblPW1");
@@ -68,7 +64,6 @@ function checkPassword1(){
 		pw1Cell.style.background = "rgb(255,233,233)";
 	}
 }
-
 function checkPassword2(){
 	var pw1Input = document.getElementById("inputPW1");
 	var pw2Input = document.getElementById("inputPW2");
@@ -86,7 +81,6 @@ function checkPassword2(){
 		pw2Cell.style.background = "rgb(255,233,233)";
 	}
 }
-
 function checkEmail(){
     var emInput = document.getElementById("inputEM");
 	var emLabel = document.getElementById("lblEM");
@@ -106,38 +100,70 @@ function checkEmail(){
 		emCell.style.background = "rgb(255,233,233)";
 	}
 }
-
 function checkPhone(){
     var pnInput = document.getElementById("inputPN");
 	var pnLabel = document.getElementById("lblPN");
 	var pnCell = document.getElementById("cellPN");
+	try{
+		if (isNaN(pnInput) === true || pnInput.value.length !== 10){
+			throw "Invalid phone number!";
+		}
+		pnCell.style.background = "";
+		pnLabel.innerHTML = "Phone Number&#40;Numbers Only&#41;";
+	}
+	catch(msg){
+		pnLabel.innerHTML = msg;
+		pnCell.style.background = "rgb(255,233,233)";
+	}
 }
-
 function checkPost(){
-	
+	var pcInput = document.getElementById("inputPC");
+	var pcInputChar = toString(pcInput);
+	var pcLabel = document.getElementById("lblPC");
+	var pcCell = document.getElementById("cellPC");
+	var cdnPost = regexCdnZip.test(pcInput);
+	var usaPost = regexUsaZip.test(pcInput);
+	try{
+		if(isNaN(pcInput) === false && pcInput.value.length !== 5){
+			if(isNaN(pcInputChar.charAt(0)) === true || isNaN(pcInputChar.charAt(2)) === true || isNaN(pcInputChar.charAt(4)) === true){
+				throw "Invalid Canada postal code!";
+			}
+			continue;
+			throw "Invalid US postal code!";
+		} else 
+		pcCell.style.background = "";
+		pcLabel.innerHTML = "Postal Code/Zip Code&#40;No space&#41;";
+	}
+	catch(msg){
+		pcLabel.innerHTML = msg;
+		pcCell.style.background = "rgb(255,233,233)"
+	}
 }
-
 function createEventListeners(){
     var fn = document.getElementById("inputFN");
     var ln = document.getElementById("inputLN");
-    var email = document.getElementById("inputEM");
+    var em = document.getElementById("inputEM");
 	var pwd1 = document.getElementById("inputPW1");
 	var pwd2 = document.getElementById("inputPW2");
 	var pn = document.getElementById("inputPN");
+	var pc = document.getElementById("inputPC");
     if (fn.addEventListener){
         fn.addEventListener("change", checkFName, false);
         ln.addEventListener("change", checkLName, false);
-        email.addEventListener("change", checkEmail, false);
+        em.addEventListener("change", checkEmail, false);
 		pwd1.addEventListener("change", checkPassword1, false);
 		pwd2.addEventListener("change", checkPassword2, false);
 		pn.addEventListener("change", checkPhone, false);
-    }else if(fn.attachEvent){
+		pc.addEventListener("change", checkPost, false);
+    }
+	else if(fn.attachEvent){
         fn.attachEvent("onchange", checkFName);
         ln.attachEvent("onchange", checkNames);
-        email.attachEvenet("onchange", checkEmail);
+        em.attachEvenet("onchange", checkEmail);
 		pwd1.attachEvenet("onchange", checkPassword1);
 		pwd2.attachEvenet("onchange", checkPassword2);
 		pn.attachEvenet("onchange", checkPhone);
+		pc.attachEvenet("onchange", checkPost);
     }
 }
 
